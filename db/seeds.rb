6e60efe312119fd2cf6b9077275921302e7a5398
Lucas Aguilar <lucasaguilar835@gmail.com>
2023-08-07 17:15:34 -0300

    Migra a database para Postgresql

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# EntityType (Malharia, Cortador, Costureira, Serigrafia, Acabamento, Vendedor, Transportadora)
+# abaixo, cria os tipos de entidades
+EntityType.create(nome: 'Malharia')
+EntityType.create(nome: 'Cortador')
+EntityType.create(nome: 'Costureira')
+EntityType.create(nome: 'Serigrafia')
+EntityType.create(nome: 'Acabamento')
+EntityType.create(nome: 'Vendedor')
+EntityType.create(nome: 'Transportadora')
